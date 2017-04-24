9614f4d2ab432ae759b37a3f49be7bec236a8414
Michael Djurdjevic <mdjurdj1@binghamton.edu>
2017-04-24 11:32:40 +0000

    Done.

--- components/Pet.js
+++ components/Pet.js
@@ -7,25 +7,24 @@ class Pet extends React.Component {
   }
 
   handleClick() {
-    this.props.onAdoptPet(this.props.id)
+    this.props.onAdoptPet(this.props.pet.id)
   }
   
   render() {
     return (
       <div className="card">
-        <div className="content">
-          <a className="header">{this.props.name} {this.props.gender === 'male' ? '♂' : '♀'}</a>
-          <div className="meta">
-            <span className="date">{this.props.type}</span>
-          </div>
-          <div className="description">
-            <p>Age: {this.props.age}</p>
-            <p>Weight: {this.props.weight}</p>
-          </div>
-        </div>
+       <div className="content">
+         <a className="header">{this.props.pet.name}{(this.props.pet.gender == 'male') ? '♂' : '♀'}</a>
+         <div className="meta">
+           <span className="date">{this.props.pet.type}</span>
+         </div>
+         <div className="description">
+           <p>Age: {this.props.pet.age}</p>
+           <p>Weight: {this.props.pet.weight}</p>
+         </div>
+       </div>
         <div className="extra content">
-          {this.props.isAdopted ? 
-          <button className="ui disabled button">Already adopted</button> : <button className="ui primary button" onClick={this.handleClick}>Adopt pet</button> }
+          {this.props.isAdopted ? <button className="ui disabled button">Already adopted</button> : <button className="ui primary button" onClick={this.handleClick}>Adopt pet</button> }
         </div>
       </div>
     );
--- components/PetBrowser.js
+++ components/PetBrowser.js
@@ -4,9 +4,12 @@ const Pet = require('./Pet');
 
 class PetBrowser extends React.Component {
   render() {
+    const renderedPets = this.props.pets.map(pet => (
+      <Pet pet={pet} onAdoptPet={this.props.onAdoptPet} isAdopted={this.props.adoptedPets.includes(pet.id)} />
+    ))
     return (
       <div className="ui cards">
-        <code>&lt;Pet /&gt;</code> &nbsp; components should go here
+        {renderedPets}
       </div>
     );
   }
