0bc7435000a282fc223a3e8197b1e639a6c126a6
Scott Savage <snsavage@gmail.com>
2017-05-10 18:50:58 -0400

    Pass all lab specs

--- components/App.js
+++ components/App.js
@@ -17,10 +17,16 @@ export default class App extends React.Component {
 
     this.onChangeType = this.onChangeType.bind(this);
     this.onFindPetsClick = this.onFindPetsClick.bind(this);
+    this.onAdoptPet = this.onAdoptPet.bind(this);
+  }
+
+  onAdoptPet(id) {
+    this.setState({
+      adoptedPets: [...this.state.adoptedPets, id]
+    })
   }
 
   onChangeType(type) {
-    console.log(type);
     this.setState({
       filters: Object.assign({}, this.state.filters, {
         type: type
@@ -28,17 +34,22 @@ export default class App extends React.Component {
     })
   }
 
-  onFindPetsClick(type) {
+  onFindPetsClick() {
     let url = "/api/pets";
+    var type = this.state.filters.type;
 
-    if(type !== 'all') { url = `${url}?type=${type}` };
+    if(type !== 'all') {
+      url = "/api/pets?type=" + type;
+    }
 
     fetch(url).then(function(response) {
+      this.setState({
+        pets: response.body.json()
+      })
     });
   }
 
   render() {
-    console.log(this.state);
     return (
       <div className="ui container">
         <header>
@@ -52,7 +63,8 @@ export default class App extends React.Component {
                 onFindPetsClick={this.onFindPetsClick} />
             </div>
             <div className="twelve wide column">
-              <PetBrowser pets={this.state.pets} />
+              <PetBrowser pets={this.state.pets}
+                onAdoptPet={this.onAdoptPet} />
             </div>
           </div>
         </div>
--- components/Filters.js
+++ components/Filters.js
@@ -6,20 +6,25 @@ class Filters extends React.Component {
 
     this.state = {value: props.filters.type};
 
+    this.updateSelect = this.updateSelect.bind(this);
   }
 
   updateSelect(ev) {
+    const value = ev.target.value;
+
+    this.setState({
+      value: value
+    }, this.props.onChangeType(value))
   }
 
   render() {
-    console.log(this.state.value);
     return (
       <div className="ui form">
         <h3>Animal type</h3>
         <div className="field">
           <select name="type" id="type"
             value={this.state.value}
-            onChange={() => this.props.onChangeType(this.state.value)}>
+            onChange={this.updateSelect}>
             <option value="all">All</option>
             <option value="cat">Cats</option>
             <option value="dog">Dogs</option>
@@ -29,7 +34,7 @@ class Filters extends React.Component {
 
         <div className="field">
           <button className="ui secondary button"
-            onClick={() => this.props.onFindPetsClick(this.state.value)}>
+            onClick={() => this.props.onFindPetsClick()}>
             Find pets
           </button>
         </div>
