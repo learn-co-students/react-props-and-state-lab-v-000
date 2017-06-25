560db8a161d90cdbe2faaab3574ad8bfd8257086
briecoyle <brie.coyle@gmail.com>
2017-06-25 17:51:49 -0400

    Done.

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Filters extends React.Component {
   constructor() {
     super();
+
+    this.handleFilterChange = this.handleFilterChange.bind(this);
+    this.handleFindPetsClick = this.handleFindPetsClick.bind(this);
+  }
+
+  handleFilterChange(event) {
+    this.props.onChangeType(event.target.value)
+  }
+
+  handleFindPetsClick(event) {
+    this.props.onFindPetsClick(event)
   }
 
   render() {
@@ -10,7 +22,7 @@ class Filters extends React.Component {
       <div className="ui form">
         <h3>Animal type</h3>
         <div className="field">
-          <select name="type" id="type">
+          <select name="type" id="type" value={this.props.filters.type} onChange={this.handleFilterChange}>
             <option value="all">All</option>
             <option value="cat">Cats</option>
             <option value="dog">Dogs</option>
@@ -19,11 +31,17 @@ class Filters extends React.Component {
         </div>
 
         <div className="field">
-          <button className="ui secondary button">Find pets</button>
+          <button className="ui secondary button" onClick={this.handleFindPetsClick}>Find pets</button>
         </div>
       </div>
     );
   }
 }
+
+Filters.propTypes = {
+  onChangeType: PropTypes.func,
+  onFindPetsClick: PropTypes.func,
+}
+
 
 export default Filters;
--- src/components/PetBrowser.js
+++ src/components/PetBrowser.js
@@ -1,15 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import Pet from './Pet';
 
 class PetBrowser extends React.Component {
   render() {
+    const pets = this.props.pets.map((pet, index) => (
+      <Pet pet={pet} key={index} onAdoptPet={this.props.onAdoptPet} isAdopted={this.props.adoptedPets.includes(pet.id)} />
+    ));
+
     return (
       <div className="ui cards">
-        <code>&lt;Pet /&gt;</code> &nbsp; components should go here
+        {pets}
       </div>
     );
   }
+}
+
+PetBrowser.propTypes = {
+  pets: PropTypes.array,
+  onAdoptPet: PropTypes.func,
+  adoptedPets: PropTypes.array,
 }
 
 export default PetBrowser;
