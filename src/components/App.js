71b2483d091da880c45cfd4584789dba4c501481
John Verdone <jmverdone@gmail.com>
2017-07-04 16:06:16 -0400

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -14,27 +14,64 @@ class App extends React.Component {
         type: 'all',
       }
     };
+    this.handleChangeFilterType = this.handleChangeFilterType.bind(this)
+    this.findPets = this.findPets.bind(this)
+    this.handleAdoptPet = this.handleAdoptPet.bind(this)
   }
 
-  render() {
-    return (
-      <div className="ui container">
-        <header>
-          <h1 className="ui dividing header">React Animal Shelter</h1>
-        </header>
-        <div className="ui container">
-          <div className="ui grid">
-            <div className="four wide column">
-              <Filters />
-            </div>
-            <div className="twelve wide column">
-              <PetBrowser />
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
+  handleChangeFilterType = e => {
+   this.setState({
+     filters: {
+       ...this.state.filters,
+       type: e
+     }
+   })
+ }
+
+ findPets() {
+   let url = '/api/pets'
+
+   if (this.state.filters.type !== 'all') {
+     url += `?type=${this.state.filters.type}`
+   }
+
+   fetch(url)
+     .then(res => res.json())
+     .then(pets => this.setState({ pets }))
+ }
+
+ handleAdoptPet = pet => {
+   this.setState({
+     adoptedPets: [...this.state.adoptedPets, pet],
+   })
+ }
+
+ render() {
+   return (
+     <div className='ui container'>
+       <header>
+         <h1 className='ui dividing header'>React Animal Shelter</h1>
+       </header>
+       <div className='ui container'>
+         <div className='ui grid'>
+           <div className='four wide column'>
+             <Filters filters={this.state.filters}
+               onChangeType={this.handleChangeFilterType}
+               onFindPetsClick={this.findPets}
+             />
+           </div>
+           <div className='twelve wide column'>
+             <PetBrowser pets={this.state.pets}
+               adoptedPets={this.state.adoptedPets}
+               onAdoptPet={this.handleAdoptPet}
+             />
+           </div>
+         </div>
+       </div>
+     </div>
+   )
+ }
+
 }
 
 export default App;
