6999fc4aa46c4f312ecc33ed9d0288ca99ea1e48
Isis Tejeda <isis@isistejeda.com>
2017-08-08 15:45:04 -0500

    Done.

--- src/components/Pet.js
+++ src/components/Pet.js
@@ -1,30 +1,59 @@
 import React from 'react';
 
-class Pet extends React.Component {
+export default class Pet extends React.Component {
   constructor() {
     super();
+    this.adoptPet = this.adoptPet.bind(this);
+  }
+
+  adoptPet(){
+    this.props.onAdoptPet(this.props.pet.id)
   }
 
   render() {
+
+    const { pet, isAdopted } = this.props;
+    const { name, type, gender, age, weight } = pet;
+
     return (
       <div className="card">
         <div className="content">
-          <a className="header">Pet name (gender: ♂ or ♀)</a>
+          <a className="header">{name} {gender === 'male' ? '♂' : '♀'}</a>
+
           <div className="meta">
             <span className="date">Pet type</span>
+            <p>{type}</p>
           </div>
           <div className="description">
-            <p>Age: </p>
-            <p>Weight: </p>
+            <p>Age: {age}</p>
+            <p>Weight: {weight} </p>
           </div>
         </div>
+        
         <div className="extra content">
-          <button className="ui primary button">Adopt pet</button>
-          <button className="ui disabled button">Already adopted</button>
+          {!isAdopted && 
+          <button className="ui primary button"
+           onClick={this.adoptPet}
+          >Adopt pet
+          </button>
+          }
+          {isAdopted && <button className="ui disabled button">Already adopted</button>}
+          
         </div>
       </div>
     );
   }
 }
 
-export default Pet;
+// Pet.propTypes = {
+//   pet: PropTypes.shape({
+//     name: PropTypes.string,
+//     type: PropTypes.string,
+//     age: PropTypes.number,
+//     weight: PropTypes.number,
+//     gender: PropTypes.oneOf(['Male', 'Female']),
+//     isAdopted: PropTypes.bool,
+//   }),
+  
+
+// };
