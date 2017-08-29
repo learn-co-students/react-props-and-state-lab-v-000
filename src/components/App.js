7bbe9c9b75e1f8fc05bf3a32f74d9d50101b990a
SiamKing <thesiameseelectric@gmail.com>
2017-08-29 14:31:52 -0700

    that was a doozy

--- src/components/App.js
+++ src/components/App.js
@@ -16,6 +16,40 @@ class App extends React.Component {
     };
   }
 
+  handleFilterChange = (type) => {
+    this.setState({
+      filters: {
+        ...this.state.filters,
+        type: type
+      }
+    })
+  }
+
+  fetchPets = () => {
+    let apiUrl = '/api/pets'
+
+    if (this.state.filters.type === 'cat') {
+      apiUrl += '?type=cat'
+    } else if (this.state.filters.type === 'dog') {
+      apiUrl += '?type=dog'
+    } else if (this.state.filters.type === 'micropig') {
+      apiUrl += '?type=micropig'
+    }
+
+    fetch(apiUrl).then(response => {
+      this.setState({
+        pets: response
+      })
+    })
+  }
+
+  addToAdoptedPets = (id) => {
+    this.setState({
+      adoptedPets: [...this.state.adoptedPets, id]
+    })
+
+  }
+
   render() {
     return (
       <div className="ui container">
@@ -25,10 +59,17 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters pets={this.state.pets}
+              adoptedPets={this.state.adoptedPets}
+              filters={this.state.filters}
+              onChangeType={this.handleFilterChange}
+              onFindPetsClick={this.fetchPets}
+              />
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser pets={this.state.pets}
+              onAdoptPet={this.addToAdoptedPets}/>
+              isAdopted={this.state.adoptedPets}
             </div>
           </div>
         </div>
--- src/data/pets.js
+++ src/data/pets.js
@@ -280,7 +280,7 @@ const allPets = [
     "name": "Hennessy"
   }
 ];
-
+export {allPets}
 export const getAll = () => allPets;
 export const getByType = type => allPets.filter(pet => pet.type === type);
 export const getBetweenAge = (from, to) => allPets.filter(pet => pet.age >= from && pet.age <= to);
