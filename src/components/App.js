a3046b27582c8238363738e4acaf1a33d0b186d8
Mikyle Crawford <crawfordmikyle@gmail.com>
2017-09-10 13:10:58 -0700

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -2,18 +2,50 @@ import React from 'react';
 
 import Filters from './Filters';
 import PetBrowser from './PetBrowser';
+import "whatwg-fetch";
 
 class App extends React.Component {
   constructor() {
     super();
 
+    // create state to keep track of
     this.state = {
       pets: [],
       adoptedPets: [],
-      filters: {
+      filters:{
         type: 'all',
       }
     };
+  }
+
+  getPets = () => {
+    var baseUrl = "/api/pets";
+
+    if(this.state.filters.type !== 'all'){
+      baseUrl += `?type=${this.state.filters.type}`
+    };
+
+    fetch(baseUrl)
+      .then(res => res.json())
+      .then(pets => {
+        console.log(pets)
+        this.setState({pets})
+      })
+  }
+
+
+  handleChangeFilterType = (type) => {
+    this.setState({
+      filters: Object.assign({}, this.state.filters, {
+        type: type,
+      })
+    });
+  }
+
+  handleAdoptPet = (petId) => {
+    this.setState({
+      adoptedPets: [...this.state.adoptedPets, petId],
+    });
   }
 
   render() {
@@ -25,10 +57,17 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters
+                filters={this.state.filters}
+                onChangeType={this.handleChangeFilterType}
+                onFindPetsClick={this.getPets}
+              />
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser pets={this.state.pets}
+                          adoptedPets={this.state.adoptedPets}
+                          onAdoptPet={this.handleAdoptPet}
+              />
             </div>
           </div>
         </div>
