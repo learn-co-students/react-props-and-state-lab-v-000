f4560a4290319d62d8b94fe9ebd73b30514ade56
James Fernandez <jffernan@comcast.net>
2017-09-21 10:41:23 -0700

    Update Pet.js

--- src/components/Pet.js
+++ src/components/Pet.js
@@ -1,11 +1,20 @@
 import React from 'react';
 
-class Pet extends React.Component {
+export default class Pet extends React.Component {
   constructor() {
     super();
+    
+    this.handleAdoptPet = this.handleAdoptPet.bind(this);
   }
 
+  handleAdoptPet() {
+    this.props.onAdoptPet(this.props.pet.id);
+  }
+  
   render() {
+    const { pet, isAdopted } = this.props;
+    const { name, type, gender, age, weight } = pet;
+    
     return (
       <div className="card">
         <div className="content">
@@ -14,17 +23,15 @@ class Pet extends React.Component {
             <span className="date">Pet type</span>
           </div>
           <div className="description">
-            <p>Age: </p>
-            <p>Weight: </p>
+            <p>Age: {age}</p>
+            <p>Weight: {weight}</p>
           </div>
         </div>
         <div className="extra content">
-          <button className="ui primary button">Adopt pet</button>
-          <button className="ui disabled button">Already adopted</button>
+          {!isAdopted && <button className="ui primary button" onClick={this.handleAdoptPet}>Adopt pet</button>}
+          {isAdopted && <button className="ui disabled button">Already adopted</button>}
         </div>
       </div>
     );
   }
 }
-
-export default Pet;
