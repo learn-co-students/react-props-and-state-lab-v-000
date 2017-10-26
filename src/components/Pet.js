0490ba0090aba08d8f816e1d47e12f55f0d60878
github username <github email address>
2017-10-26 15:54:30 -0400

    Done.

--- src/components/Pet.js
+++ src/components/Pet.js
@@ -1,21 +1,46 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Pet extends React.Component {
-  constructor() {
+  constructor(props) {
     super();
+
+    this.state = {
+      isAdopted: false,
+    }
   }
 
-  render() {
+  onAdoptPet = () => {
+    this.setState({
+      isAdopted: true,
+    });
+  }
+
+render() {
+  const gender
+
+  // whatGender = () => {
+  //   if(this.props.gender === male) {
+  //     gender = " ♂ "
+  //   }else{
+  //     gender = " ♀ "
+  //   }
+  //   return "<p> {gender} </p>"
+  // }
+  
     return (
       <div className="card">
         <div className="content">
-          <a className="header">Pet name (gender: ♂ or ♀)</a>
+          <a className="header">
+            <p> this.props.name </p>
+            whatGender()
+          </a>
           <div className="meta">
             <span className="date">Pet type</span>
           </div>
           <div className="description">
-            <p>Age: </p>
-            <p>Weight: </p>
+            <p>Age: this.props.age </p>
+            <p>Weight: this.props.weight</p>
           </div>
         </div>
         <div className="extra content">
@@ -26,5 +51,13 @@ class Pet extends React.Component {
     );
   }
 }
+
+Pet.propTypes = {
+  name: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+  gender: PropTypes.oneOf(['male','female']).isRequired,
+  age: PropTypes.number.isRequired,
+  weight: PropTypes.number.isRequired,
+};
 
 export default Pet;
