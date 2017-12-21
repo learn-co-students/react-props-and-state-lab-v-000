4cd07965d4e8402e04ed5b60335757e03687a3e4
github username <github email address>
2017-12-21 10:12:12 -0600

    starting app file

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,16 +1,31 @@
 import React from 'react';
 
 class Filters extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      type: props.filters.type
+    }
+
+  }
+
+  handleChangeType = event => {
+    this.props.onChangeType(event.target.value)
+  }
+
+  handleFindPets = event => {
+    this.props.onFindPetsClick()
   }
 
   render() {
+    let type = this.state.type
     return (
+
       <div className="ui form">
         <h3>Animal type</h3>
         <div className="field">
-          <select name="type" id="type">
+          <select name="type" id="type" value={type} onChange={this.handleChangeType}>
             <option value="all">All</option>
             <option value="cat">Cats</option>
             <option value="dog">Dogs</option>
@@ -19,7 +34,7 @@ class Filters extends React.Component {
         </div>
 
         <div className="field">
-          <button className="ui secondary button">Find pets</button>
+          <button className="ui secondary button" onClick={this.handleFindPets}>Find pets</button>
         </div>
       </div>
     );
