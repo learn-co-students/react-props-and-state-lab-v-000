ed07a24986bc07fe6c1278df8c76851d0ab84fdf
dbfarms <matt.hunger@gmail.com>
2018-01-24 21:20:32 +0000

    Done.

--- src/components/Pet.js
+++ src/components/Pet.js
@@ -1,30 +1,50 @@
+
 import React from 'react';
+import PropTypes from "prop-types";
 
 class Pet extends React.Component {
-  constructor() {
-    super();
-  }
-
+  
+  handleAdoptPet = () => this.props.onAdoptPet(this.props.pet.id)
+  
   render() {
+    const { pet: { name, type, gender, age, weight }, isAdopted } = this.props;
+    
     return (
       <div className="card">
         <div className="content">
-          <a className="header">Pet name (gender: ♂ or ♀)</a>
+          <a className="header">{name} {gender === 'male' ? '♂' : '♀'}</a>
           <div className="meta">
-            <span className="date">Pet type</span>
+            <span className="date">{type}</span>
           </div>
           <div className="description">
-            <p>Age: </p>
-            <p>Weight: </p>
+            <p>Age: {age}</p>
+            <p>Weight: {weight}</p>
           </div>
         </div>
         <div className="extra content">
-          <button className="ui primary button">Adopt pet</button>
+          {isAdopted ?
           <button className="ui disabled button">Already adopted</button>
+          :
+          <button 
+            className="ui primary button"
+            onClick={this.handleAdoptPet}
+            >
+            Adopt pet
+            </button>
+          }
         </div>
       </div>
     );
   }
+}
+
+Pet.propTypes = {
+  id: PropTypes.string,
+  type: PropTypes.string,
+  gender: PropTypes.string,
+  age: PropTypes.number,
+  weight: PropTypes.number,
+  name: PropTypes.string,  
 }
 
 export default Pet;
--- src/components/PetBrowser.js
+++ src/components/PetBrowser.js
@@ -4,12 +4,31 @@ import Pet from './Pet';
 
 class PetBrowser extends React.Component {
   render() {
+    const  renderPets = this.props.pets.map(pet => 
+      <Pet //Pet from ./Pet 
+        pet={pet}
+        key={pet.id}
+        onAdoptPet={this.props.onAdoptPet}
+        isAdopted={this.props.adoptedPets.includes(pet.id)}
+      />
+    )
+    
     return (
       <div className="ui cards">
-        <code>&lt;Pet /&gt;</code> &nbsp; components should go here
+        {renderPets}
       </div>
     );
   }
 }
 
 export default PetBrowser;
+
+
+/*
+
+Should have a pets prop. This is an array of pets that the component uses to render <Pet /> components.
+Should have an onAdoptPet prop. This callback prop gets passed to its <Pet /> children components.
+Should have an adoptedPets prop. Use this prop to figure out if a pet is adopted or not, and pass that result to the <Pet /> components in the form of an isAdopted prop.
+
+
+*/
