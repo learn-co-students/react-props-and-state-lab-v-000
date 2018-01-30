f7951d65771baa05e70b7f4dd012fd8f5716c950
ndalcin <dalcin.nicole@gmail.com>
2018-01-30 09:03:18 -0500

    Done.

--- src/components/Pet.js
+++ src/components/Pet.js
@@ -1,26 +1,35 @@
 import React from 'react';
 
 class Pet extends React.Component {
-  constructor() {
-    super();
-  }
+  constructor(props) {
+    super(props);
 
+  }
   render() {
+    const isAdopted = this.props.isAdopted
+    const onAdoptPet = this.props.onAdoptPet
+    const petId = this.props.pet.id
+
     return (
       <div className="card">
         <div className="content">
-          <a className="header">Pet name (gender: ♂ or ♀)</a>
+          <a className="header">Pet name: {this.props.pet.name}
+          {this.props.pet.gender === 'male' ? '♂' : '♀'}
+          </a>
           <div className="meta">
-            <span className="date">Pet type</span>
+            <span className="date">Pet type: {this.props.pet.type}</span>
           </div>
           <div className="description">
-            <p>Age: </p>
-            <p>Weight: </p>
+            <p>Age: {this.props.pet.age}</p>
+            <p>Weight: {this.props.pet.weight}</p>
           </div>
         </div>
         <div className="extra content">
-          <button className="ui primary button">Adopt pet</button>
-          <button className="ui disabled button">Already adopted</button>
+          { isAdopted ? (
+            <button className="ui disabled button">Already adopted</button>
+          ) : (
+            <button className="ui primary button" onClick={() => this.props.onAdoptPet(petId)}>Adopt pet</button>
+          )}
         </div>
       </div>
     );
--- src/components/PetBrowser.js
+++ src/components/PetBrowser.js
@@ -4,9 +4,12 @@ import Pet from './Pet';
 
 class PetBrowser extends React.Component {
   render() {
+    const petComponents = this.props.pets.map(pet => {
+      return <div><Pet onAdoptPet={this.onAdoptPet} isAdopted={pet.isAdopted} id={pet.id} name={pet.name} type={pet.type} gender={pet.gender} age={pet.age} weight={pet.weight} /></div>
+    })
     return (
       <div className="ui cards">
-        <code>&lt;Pet /&gt;</code> &nbsp; components should go here
+        {petComponents}
       </div>
     );
   }
