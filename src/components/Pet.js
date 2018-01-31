2603e7ddcd05aed8b764721937ab4081719b376b
Kevin Yongchul Kim <kevikim33@gmail.com>
2018-01-31 15:28:30 -0800

    pet renders props correctly

--- src/components/Pet.js
+++ src/components/Pet.js
@@ -6,16 +6,17 @@ class Pet extends React.Component {
   }
 
   render() {
+    // debugger;
     return (
       <div className="card">
         <div className="content">
-          <a className="header">Pet name (gender: ♂ or ♀)</a>
+          <a className="header">Pet name (gender): {this.props.pet.name} {this.props.pet.gender === 'male' ? '♂' : '♀'}</a>
           <div className="meta">
-            <span className="date">Pet type</span>
+            <span className="date">Pet type: {this.props.pet.type}</span>
           </div>
           <div className="description">
-            <p>Age: </p>
-            <p>Weight: </p>
+            <p>Age: {this.props.pet.age} </p>
+            <p>Weight: {this.props.pet.weight} </p>
           </div>
         </div>
         <div className="extra content">
--- src/components/PetBrowser.js
+++ src/components/PetBrowser.js
@@ -2,11 +2,21 @@ import React from 'react';
 
 import Pet from './Pet';
 
+const MALE_DOG = {
+  "id": "9e7cc723-d7f5-440d-8ead-c311e68014ee",
+  "type": "dog",
+  "gender": "male",
+  "age": 8,
+  "weight": 6,
+  "name": "Kennedy"
+};
+
+
 class PetBrowser extends React.Component {
   render() {
     return (
       <div className="ui cards">
-        <code>&lt;Pet /&gt;</code> &nbsp; components should go here
+        <Pet pet={MALE_DOG} />
       </div>
     );
   }
