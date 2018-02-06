deade73d75c3e1398db600862ae8b1b07fcc4655
jared frankel <jaredfrankel10@gmail.com>
2018-02-06 00:48:40 +0000

    Done.

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,29 +1,33 @@
 import React from 'react';
 
-class Filters extends React.Component {
-  constructor() {
-    super();
-  }
+ class Filters extends React.Component {
+   constructor() {
+      super();
+    }
+  
+   handleChangeType = (event) => {
+     this.props.onChangeType(event.target.value);
+   }
 
-  render() {
-    return (
-      <div className="ui form">
-        <h3>Animal type</h3>
-        <div className="field">
-          <select name="type" id="type">
-            <option value="all">All</option>
-            <option value="cat">Cats</option>
-            <option value="dog">Dogs</option>
-            <option value="micropig">Micropigs</option>
-          </select>
+    render() {
+      return (
+        <div className="ui form">
+          <h3>Animal type</h3>
+          <div className="field">
+           <select name="type" id="type" value={this.props.filters.type} onChange={this.handleChangeType}>
+              <option value="all">All</option>
+              <option value="cat">Cats</option>
+              <option value="dog">Dogs</option>
+             <option value="micropig">Micropigs</option>
+           </select>
+          </div>
+
+          <div className="field">
+           <button onClick={this.props.onFindPetsClick} className="ui secondary button">Find pets</button>
+          </div>
         </div>
+      );
+   }
+ }
 
-        <div className="field">
-          <button className="ui secondary button">Find pets</button>
-        </div>
-      </div>
-    );
-  }
-}
-
-export default Filters;
+ export default Filters;
--- src/components/Pet.js
+++ src/components/Pet.js
@@ -1,30 +1,50 @@
 import React from 'react';
 
-class Pet extends React.Component {
-  constructor() {
-    super();
-  }
+  class Pet extends React.Component {
+   constructor(props) {
+      super();
+    }
 
-  render() {
-    return (
-      <div className="card">
-        <div className="content">
-          <a className="header">Pet name (gender: ♂ or ♀)</a>
-          <div className="meta">
-            <span className="date">Pet type</span>
+   getGender = (gender) => {
+     if(gender === "male") {
+       return "♂";
+     } else {
+       return "♀";
+     }
+   }
+
+   handleAdoptPet = () => this.props.onAdoptPet(this.props.pet.id);
+
+   renderAdoptPet = (props) => {
+     if (props.isAdopted) {
+       return <button className="ui disabled button">Already adopted</button>;
+     } else {
+       return <button onClick={this.handleAdoptPet} className="ui primary button">Adopt pet</button>;
+     }
+   }
+
+    render() {
+     const { id, type, name, gender, age, weight } = this.props.pet;
+
+
+      return (
+        <div className="card">
+          <div className="content">
+           <a className="header">{name} {this.getGender(gender)}</a>
+            <div className="meta">
+             <span className="date">{type}</span>
+            </div>
+            <div className="description">
+             <p>Age: {age}</p>
+             <p>Weight: {weight}</p>
+            </div>
           </div>
-          <div className="description">
-            <p>Age: </p>
-            <p>Weight: </p>
+          <div className="extra content">
+           {this.renderAdoptPet(this.props)}
           </div>
         </div>
-        <div className="extra content">
-          <button className="ui primary button">Adopt pet</button>
-          <button className="ui disabled button">Already adopted</button>
-        </div>
-      </div>
-    );
-  }
-}
+      );
+   }
+ }
 
-export default Pet;
+ export default Pet;
