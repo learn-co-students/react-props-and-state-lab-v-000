aed5d82b52de652d4f05821b137e598105edd489
alpbourne <alpbourne@gmail.com>
2018-03-16 14:23:59 -0400

    Done.

--- src/components/PetBrowser.js
+++ src/components/PetBrowser.js
@@ -2,14 +2,20 @@ import React from 'react';
 
 import Pet from './Pet';
 
-class PetBrowser extends React.Component {
+export default class PetBrowser extends React.Component {
   render() {
+    let renderPets = this.props.pets.map(pet =>
+      <Pet
+        pet={pet}
+        key={pet.id}
+        onAdoptPet={this.props.onAdoptPet}
+        isAdopted={this.props.adoptedPets.includes(pet.id)}
+      />
+    )
     return (
       <div className="ui cards">
-        <code>&lt;Pet /&gt;</code> &nbsp; components should go here
+        {renderPets}
       </div>
     );
   }
 }
-
-export default PetBrowser;
