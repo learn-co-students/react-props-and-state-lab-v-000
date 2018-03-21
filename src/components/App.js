c447c70474a37bec1e687f95a4d2bf3600141a60
PRbsas <paularamirez0316@gmail.com>
2018-03-21 19:07:40 -0400

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -1,40 +1,70 @@
-import React from 'react';
+import React from 'react'
 
-import Filters from './Filters';
-import PetBrowser from './PetBrowser';
+import Filters from './Filters'
+import PetBrowser from './PetBrowser'
 
-class App extends React.Component {
-  constructor() {
-    super();
+export default class App extends React.Component {
+  constructor () {
+    super()
 
     this.state = {
       pets: [],
       adoptedPets: [],
       filters: {
-        type: 'all',
+        type: 'all'
       }
-    };
+    }
   }
 
-  render() {
+  fetchPets = () => {
+    let url = '/api/pets'
+
+    if (this.state.filters.type !== 'all') {
+      url += `?type=${this.state.filters.type}`
+    }
+
+    fetch(url)
+    .then(res => res.json())
+    .then(pets => this.setState({ pets }))
+  }
+
+  handleChangeFilterType = (type) => {
+    this.setState({
+      filters: { ...this.state.filters, type }
+    })
+  }
+
+  handleAdoptPet = (petId) => {
+    this.setState({
+      adoptedPets: [ ...this.state.adoptedPets, petId ]
+    })
+  }
+
+  render () {
     return (
-      <div className="ui container">
+      <div className='ui container'>
         <header>
-          <h1 className="ui dividing header">React Animal Shelter</h1>
+          <h1 className='ui dividing header'>React Animal Shelter</h1>
         </header>
-        <div className="ui container">
-          <div className="ui grid">
-            <div className="four wide column">
-              <Filters />
+        <div className='ui container'>
+          <div className='ui grid'>
+            <div className='four wide column'>
+              <Filters
+                filters={this.state.filters}
+                onChangeType={this.handleChangeFilterType}
+                onFindPetsClick={this.fetchPets}
+              />
             </div>
-            <div className="twelve wide column">
-              <PetBrowser />
+            <div className='twelve wide column'>
+              <PetBrowser
+                pets={this.state.pets}
+                adoptedPets={this.state.adoptedPets}
+                onAdoptPet={this.handleAdoptPet}
+              />
             </div>
           </div>
         </div>
       </div>
-    );
+    )
   }
 }
-
-export default App;
