852cb11287e25cb13d049fd99c423154e2cf127b
Eric McKinney <mckinney99@gmail.com>
2018-04-16 10:33:58 -0500

    done

--- src/components/App.js
+++ src/components/App.js
@@ -13,28 +13,73 @@ class App extends React.Component {
       filters: {
         type: 'all',
       }
-    };
-  }
+    }
 
-  render() {
-    return (
-      <div className="ui container">
-        <header>
-          <h1 className="ui dividing header">React Animal Shelter</h1>
-        </header>
-        <div className="ui container">
-          <div className="ui grid">
-            <div className="four wide column">
-              <Filters />
-            </div>
-            <div className="twelve wide column">
-              <PetBrowser />
+        this.onFindPetsClick = this.onFindPetsClick.bind(this)
+        this.onChangeType = this.onChangeType.bind(this)
+        this.onAdoptPet = this.onAdoptPet.bind(this)
+      }
+
+      onChangeType = e => {
+        this.setState({ filters: { type: e } })
+      }
+
+      onAdoptPet = petId => {
+        this.setState({
+          adoptedPets: [...this.state.adoptedPets, petId]
+        })
+      }
+
+      onFindPetsClick = () => {
+        let url = "/api/pets"
+        switch (this.state.filters.type) {
+          case "cat":
+            url = "/api/pets?type=cat"
+            break
+          case "dog":
+            url = "/api/pets?type=dog"
+            break
+          case "micropig":
+            url = "/api/pets?type=micropig"
+            break
+          default:
+            url = "/api/pets"
+        }
+
+        fetch(url)
+          .then(function(response) {
+            return response.json()
+          })
+          .then(pets => this.setState({ pets: pets }))
+      }
+
+      render() {
+        return (
+          <div className="ui container">
+            <header>
+              <h1 className="ui dividing header">React Animal Shelter</h1>
+            </header>
+            <div className="ui container">
+              <div className="ui grid">
+                <div className="four wide column">
+                  <Filters
+                    onChangeType={this.onChangeType}
+                    onFindPetsClick={this.onFindPetsClick}
+                    filters={this.state.filters}
+                  />
+                </div>
+                <div className="twelve wide column">
+                  <PetBrowser
+                    pets={this.state.pets}
+                    adoptedPets={this.state.adoptedPets}
+                    onAdoptPet={this.onAdoptPet}
+                  />
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-      </div>
-    );
-  }
-}
+        )
+      }
+    }
 
-export default App;
+    export default App
