43eefe259d09316874a6bba4709fa00ece485d67
Adjoa <adjoa.darien@gmail.com>
2018-05-22 02:11:31 +0000

    Done.

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -4,13 +4,19 @@ class Filters extends React.Component {
   constructor() {
     super();
   }
+  
+  handleTypeChange = (e) => {
+    const typeChange = e.target.value;
+    
+    this.props.onChangeType(typeChange);
+  }
 
   render() {
     return (
       <div className="ui form">
         <h3>Animal type</h3>
         <div className="field">
-          <select name="type" id="type">
+          <select name="type" id="type" value={this.props.filters.type} onChange={this.handleTypeChange}>
             <option value="all">All</option>
             <option value="cat">Cats</option>
             <option value="dog">Dogs</option>
@@ -19,7 +25,7 @@ class Filters extends React.Component {
         </div>
 
         <div className="field">
-          <button className="ui secondary button">Find pets</button>
+          <button className="ui secondary button" onClick={this.props.onFindPetsClick}>Find pets</button>
         </div>
       </div>
     );
--- src/components/PetBrowser.js
+++ src/components/PetBrowser.js
@@ -6,7 +6,15 @@ class PetBrowser extends React.Component {
   render() {
     return (
       <div className="ui cards">
-        <code>&lt;Pet /&gt;</code> &nbsp; components should go here
+        <ul>
+          {Object.keys(this.props.pets).map(key => (
+          <Pet 
+            pet={this.props.pets[key]}
+            onAdoptPet={this.props.onAdoptPet}
+            isAdopted={this.props.adoptedPets.includes(this.props.pets[key].id)}
+          />
+          ))}
+        </ul>
       </div>
     );
   }
