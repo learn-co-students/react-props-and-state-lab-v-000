48f7f3969f65be61440788493279bda693a89240
raemillz <raeanne1991@gmail.com>
2018-06-17 11:35:40 -0400

    Done.

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -5,12 +5,18 @@ class Filters extends React.Component {
     super();
   }
 
+  handleChangeType = event => this.props.onChangeType(event.target.value);
+
   render() {
     return (
       <div className="ui form">
         <h3>Animal type</h3>
         <div className="field">
-          <select name="type" id="type">
+          <select
+          name="type"
+          id="type"
+          value={this.props.filters.type}
+          onChange={this.handleChangeType}>
             <option value="all">All</option>
             <option value="cat">Cats</option>
             <option value="dog">Dogs</option>
@@ -19,7 +25,9 @@ class Filters extends React.Component {
         </div>
 
         <div className="field">
-          <button className="ui secondary button">Find pets</button>
+          <button
+          className="ui secondary button"
+          onClick={this.props.onFindPetsClick}>Find pets</button>
         </div>
       </div>
     );
--- src/components/PetBrowser.js
+++ src/components/PetBrowser.js
@@ -4,9 +4,16 @@ import Pet from './Pet';
 
 class PetBrowser extends React.Component {
   render() {
+    const allPets = this.props.pets.map (pet =>
+      <Pet
+        pet={pet}
+        key={pet.id}
+        isAdopted={this.props.adoptedPets.includes(pet.id)}
+        onAdoptPet={this.props.onAdoptPet}
+      />)
     return (
       <div className="ui cards">
-        <code>&lt;Pet /&gt;</code> &nbsp; components should go here
+        {allPets}
       </div>
     );
   }
