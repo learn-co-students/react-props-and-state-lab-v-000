1ef4225ca7988aa159fc576ecc3997de33b72e1c
github username <github email address>
2018-06-27 13:22:26 -0400

    Done.

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,12 +1,18 @@
 import React from 'react'
+import PropTypes from 'prop-types';
 
 class Filters extends React.Component {
+
+  handleChange = event => {
+    this.props.onChangeType(event.target.value)
+  }
+
   render() {
     return (
       <div className="ui form">
         <h3>Animal type</h3>
         <div className="field">
-          <select name="type" id="type">
+          <select name="type" id="type" value={this.props.filters.type} onChange={this.handleChange}>
             <option value="all">All</option>
             <option value="cat">Cats</option>
             <option value="dog">Dogs</option>
@@ -15,7 +21,7 @@ class Filters extends React.Component {
         </div>
 
         <div className="field">
-          <button className="ui secondary button">Find pets</button>
+          <button className="ui secondary button" onClick={this.props.onFindPetsClick}>Find pets</button>
         </div>
       </div>
     )
@@ -23,3 +29,7 @@ class Filters extends React.Component {
 }
 
 export default Filters
+
+Filters.propTypes = {
+  onFindPetsClick: PropTypes.func
+}
