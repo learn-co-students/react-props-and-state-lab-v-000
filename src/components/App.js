9007f8383018f5adf5fec904599adbb27b09a532
BuffaloRob <peckham.rob@gmail.com>
2018-07-12 11:39:36 -0400

    app tests passing

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 import Filters from './Filters'
 import PetBrowser from './PetBrowser'
@@ -15,6 +16,26 @@ class App extends React.Component {
     }
   }
 
+  onChangeType = event => {
+    debugger
+    this.setState({
+      filters: {
+        ...this.state.filters,
+        type: event.target.value,//how do I update dynamically?
+      }
+    }, () => console.log(this.state.filters.type))
+  }
+
+  fetchPets = () => {
+    let url = "/api/pets";
+    if (this.state.filters.type !== "all") {
+      url += `?type=${this.state.filters.type}`;
+    } 
+    fetch(url)
+      .then(res => res.json())
+      .then(pets => this.setState( { pets }) );
+  }
+
   render() {
     return (
       <div className="ui container">
@@ -24,7 +45,7 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters onChangeType={this.onChangeType} onFindPetsClick={this.fetchPets} />
             </div>
             <div className="twelve wide column">
               <PetBrowser />
@@ -35,5 +56,11 @@ class App extends React.Component {
     )
   }
 }
+
+// App.propTypes = {
+//   onChangeType: PropTypes.func,
+//   onFindPetsClick: PropTypes.func,
+
+// }
 
 export default App
