0344913c55519f75bd920a4f0a5eb749394eda08
seanoughton <seanoughton@gmail.com>
2018-08-09 15:51:46 -0700

    Done.

--- src/components/Pet.js
+++ src/components/Pet.js
@@ -1,14 +1,4 @@
-//Should receive a pet prop. Use the attributes in this data to render the pet card correctly. It should show the pet's name, type, age and weight. Based on the pet's gender, the component also needs to contain either a male (♂) or female (♀) symbol.
-
-//Should receive an isAdopted prop. Using this prop, render the correct button in the pet's card; if the pet is adopted, show the disabled button. Otherwise, show the primary button to adopt the pet.
-
-//Should receive an onAdoptPet callback prop. This callback prop gets called with the pet's id when the user clicks the adopt pet button — not when they click the disabled button!
-
-
 import React from 'react'
-
-
-
 
 class Pet extends React.Component {
 
@@ -22,16 +12,14 @@ class Pet extends React.Component {
         <div className="content">
           <a className="header">
             Gender:{this.props.pet.gender === 'male' ? '♂' : '♀'}<br></br>
-            {/*'♀' OR '♂' */}
-            {/*PET NAME*/}
             Name: {this.props.pet.name}
           </a>
           <div className="meta">
             <span className="date">{/*PET TYPE */}Type: {this.props.pet.type}< /span>
           </div>
           <div className="description">
-            <p>Age: {/*PET AGE*/} {this.props.pet.age}</p>
-            <p>Weight: {/*PET WEIGHT*/} {this.props.pet.weight}</p>
+            <p>Age: {this.props.pet.age}</p>
+            <p>Weight: {this.props.pet.weight}</p>
           </div>
         </div>
         <div className="extra content">
--- src/components/PetBrowser.js
+++ src/components/PetBrowser.js
@@ -1,18 +1,9 @@
-//Should receive a pets prop. This is an array of pets that the component uses to render <Pet /> components. App should determine which pets to pass down as props.
-//App should be responsible for filtering this list based on the types of pets the user wants to see.
-
-//Should receive an onAdoptPet prop. This callback prop gets passed to its <Pet /> children components.
-
 
 import React from 'react'
 
 import Pet from './Pet'
 
 class PetBrowser extends React.Component {
-
-  
-
-
   render() {
     const pets = this.props.pets
     return (
