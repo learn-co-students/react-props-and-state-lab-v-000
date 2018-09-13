e0e4f47b32863198a461818c6fff8bd307423d58
njpowers7915 <njpowers7915@gmail.com>
2018-09-13 08:12:14 -0400

    Done.

--- src/components/Pet.js
+++ src/components/Pet.js
@@ -1,25 +1,51 @@
 import React from 'react'
 
 class Pet extends React.Component {
+  constructor(props) {
+    super (props)
+    this.state = {
+      adoptedStatus: this.props.isAdopted
+    }
+  }
+  gender = () => {
+    if (this.props.gender == 'male') {
+      return '♂'
+    } else if (this.props.gender == 'female') {
+      return '♀'
+    }
+  }
+
+  renderAdoptButton = () => {
+    if (this.state.adoptedStatus == true) {
+
+    }
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      adoptedStatus: true
+    })
+  }
+
   render() {
     return (
       <div className="card">
         <div className="content">
           <a className="header">
-            {/*'♀' OR '♂' */}
-            PET NAME
+            {this.gender}
+            {this.props.name}
           </a>
           <div className="meta">
-            <span className="date">PET TYPE</span>
+            <span className="date">{this.props.type}</span>
           </div>
           <div className="description">
-            <p>Age: PET AGE</p>
-            <p>Weight: PET WEIGHT</p>
+            <p>Age: {this.props.age}</p>
+            <p>Weight: {this.props.weight}</p>
           </div>
         </div>
         <div className="extra content">
           <button className="ui disabled button">Already adopted</button>
-          <button className="ui primary button">Adopt pet</button>
+          <button className="ui primary button" onClick={this.handleChange}>Adopt pet</button>
         </div>
       </div>
     )
