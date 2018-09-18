e8317fc020e140ee41973033e1f9a43325711809
github username <purnima.nyc@gmail.com>
2018-09-18 19:54:07 -0400

    Done.

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,12 +1,12 @@
 import React from 'react'
 
-class Filters extends React.Component {
-  render() {
-    return (
+const Filters = ({ onChangeType, onFindPetsClick }) => (
+  
       <div className="ui form">
         <h3>Animal type</h3>
         <div className="field">
-          <select name="type" id="type">
+    
+          <select name="type" id="type" onChange={onChangeType}>
             <option value="all">All</option>
             <option value="cat">Cats</option>
             <option value="dog">Dogs</option>
@@ -15,11 +15,9 @@ class Filters extends React.Component {
         </div>
 
         <div className="field">
-          <button className="ui secondary button">Find pets</button>
+          <button onClick={onFindPetsClick} className="ui secondary button">Find pets</button>
         </div>
       </div>
-    )
-  }
-}
+    );
 
-export default Filters
+export default Filters;
