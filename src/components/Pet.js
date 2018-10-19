358e6672820988d9207b88d4bdea1e7ed1cd51e8
Donna <donnamcla@gmail.com>
2018-10-19 09:55:07 -0400

    Done.

--- src/components/Pet.js
+++ src/components/Pet.js
@@ -1,29 +1,30 @@
 import React from 'react'
+import '../card-style.css'
 
-class Pet extends React.Component {
-  render() {
-    return (
-      <div className="card">
-        <div className="content">
-          <a className="header">
-            {/*'♀' OR '♂' */}
-            PET NAME
-          </a>
-          <div className="meta">
-            <span className="date">PET TYPE</span>
-          </div>
-          <div className="description">
-            <p>Age: PET AGE</p>
-            <p>Weight: PET WEIGHT</p>
-          </div>
+const Pet = ({ pet: { id, name, type, gender, age, weight, isAdopted }, onAdoptPet }) => {
+  return (
+    <div className="card">
+      <div className="content">
+        <a className="header">
+          <strong>{name}</strong> {gender === 'female' ? '♀' : '♂'}
+        </a>
+        <div className="meta">
+          <span className="date"><small>{type}</small></span>
         </div>
-        <div className="extra content">
-          <button className="ui disabled button">Already adopted</button>
-          <button className="ui primary button">Adopt pet</button>
+        <div className="description">
+          <p>Age: {age}</p>
+          <p>Weight: {weight}</p>
         </div>
       </div>
-    )
-  }
+      <div className="extra content">
+        {isAdopted ? (
+          <button className="ui disabled button">Already adopted</button>
+        ) : (
+          <button onClick={() => onAdoptPet(id)} className="ui primary button">Adopt pet</button>
+        )}
+      </div>
+    </div>
+  )
 }
 
 export default Pet
