71fdadb853961b680455783782653c6827346dc1
Chris Alberti <christopherjalberti@gmail.com>
2018-10-25 14:53:10 -0400

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -15,6 +15,40 @@ class App extends React.Component {
     }
   }
 
+  handleSelectChange = event => {
+    this.setState({
+      filters: {
+        type: event.target.value
+      }
+    })
+  }
+
+  handleFindPetsClick = () => {
+    const selection = this.state.filters.type;
+    let url;
+
+    if (selection === "cat") {
+      url = "/api/pets?type=cat"
+    } else if (selection === "dog") {
+      url = "/api/pets?type=dog"
+    } else if (selection === "micropig") {
+      url = "/api/pets?type=micropig"
+    } else {
+      url = "/api/pets"
+    }
+
+    fetch(url)
+      .then(res => res.json())
+      .then(pets => this.setState({ pets }));
+  }
+
+  handleAdoption = id => {
+    const pet = this.state.pets.find(function(pet) {
+      return pet.id === id
+    })
+    pet.isAdopted = !pet.isAdopted
+  }
+
   render() {
     return (
       <div className="ui container">
@@ -24,10 +58,10 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters onChangeType={this.handleSelectChange} onFindPetsClick={this.handleFindPetsClick}/>
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser onAdoptPet={this.handleAdoption} pets={this.state.pets}/>
             </div>
           </div>
         </div>
