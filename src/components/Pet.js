b84e54506701441baa93b6de766e86a2b6a69585
Sonia Duarte Nicas <sduarte22@gmail.com>
2018-11-06 14:01:38 -0800

    Done.

--- src/components/Pet.js
+++ src/components/Pet.js
@@ -1,25 +1,38 @@
 import React from 'react'
 
+
 class Pet extends React.Component {
+  constructor(props){
+    super(props)
+    this.state={
+      name: props.pet.name,
+      age: props.pet.age,
+      weight: props.pet.weight,
+      gender: props.pet.gender,
+      type: props.pet.type,
+      isAdopted: props.pet.isAdopted,
+      id: props.pet.id
+    }
+  }
+
   render() {
     return (
       <div className="card">
         <div className="content">
           <a className="header">
-            {/*'♀' OR '♂' */}
-            PET NAME
+          {this.state.name}{this.state.gender === 'female' ? '♀' : '♂' }
           </a>
           <div className="meta">
-            <span className="date">PET TYPE</span>
+            <span className="date">{this.state.type}</span>
           </div>
           <div className="description">
-            <p>Age: PET AGE</p>
-            <p>Weight: PET WEIGHT</p>
+            <p>Age: {this.state.age}</p>
+            <p>Weight: {this.state.weight}</p>
           </div>
         </div>
         <div className="extra content">
-          <button className="ui disabled button">Already adopted</button>
-          <button className="ui primary button">Adopt pet</button>
+          {this.state.isAdopted ? <button className="ui disabled button">Already adopted</button> :
+          <button className="ui primary button" onClick={()=>this.props.onAdoptPet(this.state.id)}>Adopt pet</button>}
         </div>
       </div>
     )
--- src/components/PetBrowser.js
+++ src/components/PetBrowser.js
@@ -3,8 +3,13 @@ import React from 'react'
 import Pet from './Pet'
 
 class PetBrowser extends React.Component {
+
   render() {
-    return <div className="ui cards">PET COMPONENT SHOULD GO HERE</div>
+    const petsComponents = this.props.pets.map((pet, index)=>{
+      return (<Pet key={index} pet={pet} onAdoptPet={this.props.onAdoptPet}/>)
+    })
+    return <div className="ui cards">{petsComponents}</div>
+
   }
 }
 
