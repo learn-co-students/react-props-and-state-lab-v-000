27463ebc071d9fbd008a6e50c9be47d42139c0c0
github username <github email address>
2018-11-25 16:26:37 -0800

    Done.

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -6,16 +6,16 @@ class Filters extends React.Component {
       <div className="ui form">
         <h3>Animal type</h3>
         <div className="field">
-          <select name="type" id="type">
-            <option value="all">All</option>
+          <select onChange={this.props.onChangeType} name="type" id="type">
+            <option  value="all">All</option>
             <option value="cat">Cats</option>
-            <option value="dog">Dogs</option>
-            <option value="micropig">Micropigs</option>
+            <option  value="dog">Dogs</option>
+            <option  value="micropig">Micropigs</option>
           </select>
         </div>
 
         <div className="field">
-          <button className="ui secondary button">Find pets</button>
+          <button onClick={this.props.onFindPetsClick} className="ui secondary button">Find pets</button>
         </div>
       </div>
     )
