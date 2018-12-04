f32e723ac94881f85a3a4609d15cb3e1d2f9d489
Sebastian <sebgertz@gmail.com>
2018-12-04 17:43:24 -0500

    all tests passing, works in browser

--- src/components/App.js
+++ src/components/App.js
@@ -15,6 +15,30 @@ class App extends React.Component {
     }
   }
 
+  onChangeType = (e) => {
+  this.setState({
+    filters: { ...this.state.filters,
+       type: e.target.value }
+   });
+}
+  onFindPetsClick = () => {
+    let url;
+    if (this.state.filters.type === 'all') {
+      url = '/api/pets';
+    } else {
+      url = `/api/pets?type=${this.state.filters.type}`;
+    }
+    fetch(url).then(response => response.json()).then(pets => this.setState({pets: pets}))
+  }
+
+
+  onAdoptPet = id => {
+    const pets = this.state.pets.map(pet => {
+      return pet.id === id ? { ...pet, isAdopted: true } : pet;
+    });
+    this.setState({ pets });
+  };
+
   render() {
     return (
       <div className="ui container">
@@ -24,10 +48,10 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters onChangeType={this.onChangeType} onFindPetsClick={this.onFindPetsClick}/>
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser pets={this.state.pets} onAdoptPet={this.onAdoptPet}/>
             </div>
           </div>
         </div>
