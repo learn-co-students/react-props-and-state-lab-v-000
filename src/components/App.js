a7da6a82d2196bbe9fb4e034653b32100d58910f
“PetyaDjanovska” <petya.djanovska@gmail.com>
2019-01-03 14:22:06 -0500

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -24,6 +24,24 @@ class App extends React.Component {
     console.log(`Change was trigerred, type is ${this.state.filters.type}, the passed type was ${passedType}`)
   }
 
+  onFindPetsClick = () => {
+    console.log("find pets was clicked")
+    if (this.state.filters.type === "all") {
+      fetch('/api/pets').then(response => response.json()).then(data => this.setState({pets: data}));
+    } else {
+      let url = '/api/pets?type=' + this.state.filters.type;
+      fetch(url).then(response => response.json()).then(data => this.setState({pets: data}));
+    }
+    console.log(this.state.pets)
+  }
+
+  onAdoptPet = (id) => {
+    let updatedPets = this.state.pets.map(pet => {
+      return pet.id === id ? {...pet, isAdopted: true} : pet
+    });
+    this.setState({pets: updatedPets});
+  }
+
   render() {
     return (
       <div className="ui container">
@@ -33,10 +51,10 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters onChangeType={this.onChangeType} state={this.state}/>
+              <Filters onChangeType={this.onChangeType} onFindPetsClick={this.onFindPetsClick}/>
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser pets={this.state.pets} onAdoptPet={this.onAdoptPet}/>
             </div>
           </div>
         </div>
--- src/components/Filters.js
+++ src/components/Filters.js
@@ -2,26 +2,9 @@ import React from 'react'
 
 class Filters extends React.Component {
 
-  constructor(props) {
-    super(props);
-  }
-
   passValue = (e) => {
     console.log(e.target.value)
     this.props.onChangeType(e.target.value);
-  }
-
-  onFindPetsClick = () => {
-    console.log(this.props.state)
-    // if (this.props.state.filters.type === "all") {
-    //   fetch('http://localhost:3000/pets').then(response => response.json()).then(data => this.props.setState({pets: data}));
-    // } else if (this.props.state.filters.type === "dog") {
-    //   fetch('http://localhost:3000/pets?type=dog').then(response => response.json()).then(data => this.props.setState({pets: data}));
-    // } else if (this.props.state.filters.type === "cat") {
-    //   fetch('http://localhost:3000/pets?type=cat').then(response => response.json()).then(data => this.props.setState({pets: data}));
-    // } else {
-    //   fetch('http://localhost:3000/pets?type=micropigs').then(response => response.json()).then(data => this.props.setState({pets: data}));
-    // }
   }
 
   render() {
@@ -38,7 +21,7 @@ class Filters extends React.Component {
         </div>
 
         <div className="field">
-          <button className="ui secondary button" onClick={this.onFindPetsClick}>Find pets</button>
+          <button className="ui secondary button" onClick={this.props.onFindPetsClick}>Find pets</button>
         </div>
       </div>
     )
