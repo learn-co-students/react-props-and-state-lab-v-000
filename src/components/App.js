c7095fefa1010fbf31ed4fb54f2ab0d842122a2f
terryblue99 <terryblue@comcast.net>
2019-01-09 16:58:26 -0600

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -15,12 +15,8 @@ class App extends React.Component {
     }
   }
 
-  handlePetChange = (pet) => {
-    this.setState({
-      filters: {
-        type: pet.value
-      }
-    })
+  onChangeType = ({ target: { value } }) => {
+    this.setState({ filters: { ...this.state.filters, type: value } })
   }
 
   onFindPetsClick = () => {
@@ -35,6 +31,13 @@ class App extends React.Component {
       .then(pets => this.setState({ pets }))
   }
 
+  onAdoptPet = petId => {
+    const pets = this.state.pets.map(pet => {
+      return pet.id === petId ? { ...pet, isAdopted: true } : pet
+    })
+    this.setState({ pets })
+  }
+
   render() {
     return (
       <div className="ui container">
@@ -44,10 +47,10 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters onChangeType={this.onChangeType} onFindPetsClick={this.onFindPetsClick} />
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser pets={this.state.pets} onAdoptPet={this.onAdoptPet} />
             </div>
           </div>
         </div>
