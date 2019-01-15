eb5710106df6b3747b0f9a5c1fed1815e0050dee
pracniscate <lorena.giddens@outlook.com>
2019-01-14 21:09:30 -0500

    done

--- src/components/App.js
+++ src/components/App.js
@@ -15,6 +15,34 @@ class App extends React.Component {
     }
   }
 
+  // App should pass a callback prop, onChangeType, to <Filters />. This callback needs to update <App />'s state.filters.type
+  onChangeType = ({ target: { value } }) => {
+    this.setState({ filters: { ...this.state.filters, type: value } });
+  }
+
+  // <Filters /> needs a callback prop, onFindPetsClick. When the <Filters /> component calls onFindPetsClick, <App /> should fetch a list of pets using fetch().
+  fetchPets = () => {
+    // If the type is 'all', send a request to /api/pets
+    let endpoint = '/api/pets';
+
+    // If the type is 'cat', send a request to /api/pets?type=cat. Do the same thing for dog and micropig.
+    if (this.state.filters.type !== 'all') {
+      endpoint += `?type=${this.state.filters.type}`;
+    }
+
+    // Finally set <App/>'s state.pets with the results of your fetch request so you can pass the pet data down as props to <PetBrowser />
+    fetch(endpoint)
+      .then(res => res.json())
+      .then(pets => this.setState({ pets }));
+  }
+
+  onAdoptPet = petId => {
+    const pets = this.state.pets.map(p => {
+      return p.id === petId ? { ...p, isAdopted: true } : p;
+    });
+    this.setState({ pets });
+  }
+
   render() {
     return (
       <div className="ui container">
@@ -24,10 +52,10 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters onChangeType={this.onChangeType} onFindPetsClick={this.fetchPets} />
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser pets={this.state.pets} onAdoptPet={this.onAdoptPet} />
             </div>
           </div>
         </div>
