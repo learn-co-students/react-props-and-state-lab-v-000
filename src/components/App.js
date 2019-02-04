74ec5e6e9ba409a41420ce60a4ce1b551ad9bd79
Vibraniumforge <gregm9999@gmail.com>
2019-02-04 14:30:19 -0800

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -15,6 +15,24 @@ class App extends React.Component {
     }
   }
 
+  onChangeType=(event)=> {
+    this.setState({filters: {...this.state.filters, type: event.target.value }});
+  }
+
+  getPets() {
+    let url='api/pets';
+    if(this.state.filters.type !== 'all') {
+      endpoint += `?type=${this.state.filters.type}`;
+    }
+    fetch(url).then(res => res.json()).then(pets => this.setState({pets})).catch(err=> console.log("Error"));
+  }
+  
+  onFindPetsClick=(event)=> {
+    
+    
+    
+  }
+
   render() {
     return (
       <div className="ui container">
@@ -24,10 +42,10 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters onChangeType={this.onChangeType} onFindPetsClick={this.onFindPetsClick}/>
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser pets={this.state.pets} onAdoptPet ={this.onAdoptPet } />
             </div>
           </div>
         </div>
