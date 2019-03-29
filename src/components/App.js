59ef4cb41fc0ab0a474267988132b3d75e4cd599
github username <github email address>
2019-03-28 22:07:38 -0700

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -15,6 +15,17 @@ class App extends React.Component {
     }
   }
 
+  onChangeType = (event) => {
+    this.setState({
+      filters: {
+        ...this.state.filters,
+        type: event
+      }
+    })
+  }
+
+
+
   render() {
     return (
       <div className="ui container">
@@ -24,7 +35,7 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters onChangeType={this.onChangeType}/>
             </div>
             <div className="twelve wide column">
               <PetBrowser />
--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,12 +1,33 @@
 import React from 'react'
 
 class Filters extends React.Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      type: ""
+    };
+  }
+  onFindPetsClick = () => {
+    console.log(this.state)
+
+    this.props.onChangeType(this.state.type)
+  }
+
+  handleSelectedChange = event => {
+    console.log("event:" + event.target.value)
+    this.setState({
+      type: event.target.value
+    })
+}
+
   render() {
     return (
       <div className="ui form">
         <h3>Animal type</h3>
         <div className="field">
-          <select name="type" id="type">
+          <select name="type" id="type" onChange={this.handleSelectedChange}>
             <option value="all">All</option>
             <option value="cat">Cats</option>
             <option value="dog">Dogs</option>
@@ -15,7 +36,7 @@ class Filters extends React.Component {
         </div>
 
         <div className="field">
-          <button className="ui secondary button">Find pets</button>
+          <button className="ui secondary button" onClick={event => this.onFindPetsClick(event)}>Find pets</button>
         </div>
       </div>
     )
