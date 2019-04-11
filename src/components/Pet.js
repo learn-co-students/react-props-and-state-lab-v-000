5b32553c34187ff4b78dd599f86b96769311bc67
Brad Schrag <brad.schrag@gmail.com>
2019-04-11 13:26:19 -0500

    Done.

--- src/components/Pet.js
+++ src/components/Pet.js
@@ -1,31 +1,18 @@
 import React from 'react'
 
 class Pet extends React.Component {
-  constructor(props) {
-    super(props);
 
-    this.state = {
-      name: props.pet.name,
-      type: props.pet.type,
-      age: props.pet.age,
-      weight: props.pet.weight,
-      gender: props.pet.gender,
-      isAdopted: props.pet.isAdopted,
-      onAdoptPet: props.onAdoptPet,
-      id: props.pet.id
-    }
-
-  }
+  pet = this.props.pet
 
   returnGender = () => {
     let symbol = '♀'
-    if (this.state.gender === 'male') { symbol = '♂' }
+    if (this.pet.gender === 'male') { symbol = '♂' }
 
     return <div className='gender'>{symbol}</div>
   }
 
   returnAdoptionButton = () => {
-    if (this.state.isAdopted) {
+    if (this.pet.isAdopted) {
       return <button className="ui disabled button">Already adopted</button>      
     } else {
       return <button 
@@ -37,7 +24,7 @@ class Pet extends React.Component {
   }
 
   handleAdoptPetClick = () => {
-    this.state.onAdoptPet(this.state.id)
+    this.props.onAdoptPet(this.pet.id)
   }
 
   render() {
@@ -46,14 +33,14 @@ class Pet extends React.Component {
         <div className="content">
           <a className="header">
             { this.returnGender() }
-            { this.state.name }
+            { this.pet.name }
           </a>
           <div className="meta">
-            <span className="date">{this.state.type}</span>
+            <span className="date">{this.pet.type}</span>
           </div>
           <div className="description">
-            <p>Age: {this.state.age}</p>
-            <p>Weight: {this.state.weight}</p>
+            <p>Age: {this.pet.age}</p>
+            <p>Weight: {this.pet.weight}</p>
           </div>
         </div>
         <div className="extra content">
