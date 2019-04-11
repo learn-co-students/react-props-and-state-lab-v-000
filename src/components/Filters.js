dc10c5d42e4913855e65313eabb06dc2acae4c0c
Brad Schrag <brad.schrag@gmail.com>
2019-04-11 13:02:08 -0500

    Done.

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,12 +1,32 @@
 import React from 'react'
 
 class Filters extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      onChangeType: this.props.onChangeType,
+      onFindPetsClick: this.props.onFindPetsClick
+    }
+  }
+
+  handleClick = () => {
+    this.state.onFindPetsClick()
+  }
+
+  handleChange = (e) => {
+    this.state.onChangeType(e.target.value)
+  }
+
   render() {
     return (
       <div className="ui form">
         <h3>Animal type</h3>
         <div className="field">
-          <select name="type" id="type">
+          <select 
+            name="type" 
+            id="type"
+            onChange={ (e) => this.handleChange(e) } >
             <option value="all">All</option>
             <option value="cat">Cats</option>
             <option value="dog">Dogs</option>
@@ -15,7 +35,13 @@ class Filters extends React.Component {
         </div>
 
         <div className="field">
-          <button className="ui secondary button">Find pets</button>
+          <button 
+            className="ui secondary button"
+            onClick={ this.handleClick }>
+              
+              Find pets
+          
+          </button>
         </div>
       </div>
     )
--- src/components/Pet.js
+++ src/components/Pet.js
@@ -1,25 +1,63 @@
 import React from 'react'
 
 class Pet extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: props.pet.name,
+      type: props.pet.type,
+      age: props.pet.age,
+      weight: props.pet.weight,
+      gender: props.pet.gender,
+      isAdopted: props.pet.isAdopted,
+      onAdoptPet: props.onAdoptPet,
+      id: props.pet.id
+    }
+
+  }
+
+  returnGender = () => {
+    let symbol = '♀'
+    if (this.state.gender === 'male') { symbol = '♂' }
+
+    return <div className='gender'>{symbol}</div>
+  }
+
+  returnAdoptionButton = () => {
+    if (this.state.isAdopted) {
+      return <button className="ui disabled button">Already adopted</button>      
+    } else {
+      return <button 
+                  className="ui primary button"
+                  onClick={ this.handleAdoptPetClick }>
+                Adopt pet
+              </button> 
+    }
+  }
+
+  handleAdoptPetClick = () => {
+    this.state.onAdoptPet(this.state.id)
+  }
+
   render() {
     return (
       <div className="card">
         <div className="content">
           <a className="header">
-            {/*'♀' OR '♂' */}
-            PET NAME
+            { this.returnGender() }
+            { this.state.name }
           </a>
           <div className="meta">
-            <span className="date">PET TYPE</span>
+            <span className="date">{this.state.type}</span>
           </div>
           <div className="description">
-            <p>Age: PET AGE</p>
-            <p>Weight: PET WEIGHT</p>
+            <p>Age: {this.state.age}</p>
+            <p>Weight: {this.state.weight}</p>
           </div>
         </div>
         <div className="extra content">
-          <button className="ui disabled button">Already adopted</button>
-          <button className="ui primary button">Adopt pet</button>
+          { this.returnAdoptionButton() }
         </div>
       </div>
     )
