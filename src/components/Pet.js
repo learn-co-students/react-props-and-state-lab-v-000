4517c8d69bea0dcadfcb0576e4bb39c8cae0df5e
Jayon Kim <jpkim921@gmail.com>
2019-04-25 01:05:27 -0400

    added another way to use callback props in Pet.js

--- src/components/Pet.js
+++ src/components/Pet.js
@@ -30,10 +30,15 @@ class Pet extends React.Component {
             <button className="ui disabled button">Already adopted</button>
           ) : (
             <button
-              onClick={() => this.props.onAdoptPet(this.props.pet.id)}
+              onClick={this.props.onAdoptPet.bind(this, this.props.pet.id)}
               className="ui primary button">
               Adopt pet
             </button>
+            // <button
+            //   onClick={() => this.props.onAdoptPet(this.props.pet.id)}
+            //   className="ui primary button">
+            //   Adopt pet
+            // </button>
           )}
         </div>
       </div>
