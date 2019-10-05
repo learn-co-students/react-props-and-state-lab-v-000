75e960ee9c405966d64e995bf78a9627d263a7c0
Pamela Frieze Torres-Rocca <pamela@Morpheus.local>
2019-10-05 00:56:32 -0400

    Done.

--- src/components/Pet.js
+++ src/components/Pet.js
@@ -6,8 +6,8 @@ class Pet extends React.Component {
       <div className="card">
         <div className="content">
           <a className="header">
-            {/*'♀' OR '♂' */}
             {this.props.pet.name}
+            {this.props.pet.gender === 'female' ? '♀' : '♂'}
           </a>
           <div className="meta">
             <span className="date">{this.props.pet.type}</span>
