996a69b3391b2a132755d5eb8ff000c9f0b32473
Heshie Brody <4hbrody@gmail.com>
2019-11-20 14:48:42 -0500

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -15,6 +15,53 @@ class App extends React.Component {
     }
   }
 
+  onChangeType = (event) => {
+    console.log(event)
+      this.setState({
+        filters: {
+          type: event.target.value
+        }
+      }
+    )
+  }
+
+  onFindPetsClick = () => {
+    let url = `/api/pets`
+
+    if (this.state.filters.type !== 'all') {
+      url = "/api/pets?type="
+      url += this.state.filters.type
+    }
+
+    fetch(url)
+    .then(response => response.json())
+    .then(
+      (json) => {
+        this.setState({
+          pets: json
+        });
+      },
+      (error) => {
+        console.log(error.message)
+      }
+  );
+  }
+
+  onAdoptPet = (petId) => {
+    let pets = this.state.pets
+
+    pets = pets.map((pet) => {
+      if (pet.id === petId) {
+        pet.isAdopted = true
+      }
+      return pet
+    })
+
+    this.setState({
+      pets: pets
+    })
+  }
+
   render() {
     return (
       <div className="ui container">
@@ -24,10 +71,10 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters onChangeType={this.onChangeType} onFindPetsClick={this.onFindPetsClick} />
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser pets={this.state.pets} onAdoptPet={this.onAdoptPet} />
             </div>
           </div>
         </div>
--- src/components/PetBrowser.js
+++ src/components/PetBrowser.js
@@ -3,8 +3,24 @@ import React from 'react'
 import Pet from './Pet'
 
 class PetBrowser extends React.Component {
+
+  makePets = () => {
+    let newPets = this.props.pets.map((myPet, idx) => <Pet pet={myPet} onAdoptPet={this.props.onAdoptPet} key={idx}/>)
+    return newPets
+  }
+
+  logToConsole = (text) => {
+
+    console.log("called" + text.name)
+  }
+
   render() {
-    return <div className="ui cards">PET COMPONENT SHOULD GO HERE</div>
+    return (
+      <div className="ui cards">
+        {this.makePets()}
+      </div>
+
+    )
   }
 }
 
