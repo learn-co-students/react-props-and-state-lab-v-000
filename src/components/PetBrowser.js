776d8dc7b7a9bba6aa0266cbb4cb083b5af5eaf0
Rebecca Tong <rtong@buffalo.edu>
2020-04-02 15:17:06 -0400

    Done.

--- src/components/PetBrowser.js
+++ src/components/PetBrowser.js
@@ -3,8 +3,19 @@ import React from 'react'
 import Pet from './Pet'
 
 class PetBrowser extends React.Component {
+
   render() {
-    return <div className="ui cards">PET COMPONENT SHOULD GO HERE</div>
+    // create const render pets, using map,
+    // render one pet component, and pass pet object,invoke the constant inside thereturn statement
+    const pets = this.props.pets.map(animal => {
+      return <Pet pet={animal} onAdoptPet={this.props.onAdoptPet}/>
+    })
+
+    return (
+      <div className="ui cards">
+        {pets}
+      </div>
+    )
   }
 }
 
