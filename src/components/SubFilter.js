db40630683a9c47168e1c1579f13fe5a2ce1edd6
Richard-Burd <qalbalassadalasfour@gmail.com>
2020-04-20 09:58:53 -0700

    Done.

--- src/components/SubFilter.js
+++ src/components/SubFilter.js
@@ -1,8 +1,7 @@
-// Left off on this video here:
+// Additional code is based off of this video here:
 // https://www.youtube.com/watch?v=dGcsHMXbSOA
-// ...at 17:04 of 43:18
 
-import React from 'react';
+import React, {useState} from 'react';
 import Hello from './sayHello';
 import Tweet from './Tweet';
 
@@ -10,9 +9,35 @@ let mySubFilter = () => {
   console.log("The SubFilter Button was pressed!");
 }
 
-class SubFilter extends React.Component {
-  render() {
-    return (
+function SubFilter() {
+
+  // the first array variable "isRed" is the name
+  // the second array variable "setRed" is what you call to change the state
+  // value from false to true
+              //set - is a proto-keyword...this value must start with "set"
+  const [isRed, setRed] = useState(false);
+
+              //set - is a proto-keyword...this value must start with "set"
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+    setRed(!isRed)
+  }
+
+  const [users, setUsers] = useState([
+    {name: "Ron", message: "Hi, I'm Ron"},
+    {name: "Tom", message: "Hi, I'm Tom"},
+    {name: "Don", message: "Hi, I'm Don"}
+  ]);
+
+  return (
+    <div>
+      <div>
+        {users.map(user => (
+          <Tweet name={user.name} message={user.message}/>
+        ))}
+      </div>
       <div className="sub-filter">
         <button onClick={mySubFilter}>SubFilter</button>
         <Hello />
@@ -20,8 +45,16 @@ class SubFilter extends React.Component {
         <Tweet name="2nd Tweet" message="This is another tweet"/>
         <Tweet name="3rd Tweet" message="This is the final tweet"/>
       </div>
-    );
-  }
+      <div className="app">
+        {/* Now we need to add the conditional (or ternary operator) "isRed" */}
+        {/* if the "isRed" is true, then the className is 'red' and you get the css styling for it */}
+        {/* the ":" means 'else' in JSX, so if it is not true, you get no class name "" instead */}
+        <h1 className={isRed ? 'red' : ""}>Change my Color!</h1>
+        <button onClick={increment}>Increment</button>
+        <h1>{count}</h1>
+      </div>
+    </div>
+  );
 }
 
 export default SubFilter;
