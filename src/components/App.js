149eb9ce222c5568e7fe23adca031817abe63120
sdcro <sdcrouse91@gmail.com>
2020-07-22 21:42:25 -0700

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -42,9 +42,20 @@ class App extends React.Component {
   adoptPet = petId => { 
     // This technically passes the tests, but I think I need to update the state.
     // Right now, the disabled adoption button won't render.
-    const adoptedPet = this.state.pets.find(pet => pet.id === petId);
+    // const adoptedPet = this.state.pets.find(pet => pet.id === petId);
+    // adoptedPet.isAdopted = true;
+    // console.log(adoptedPet);
+
+    let petsToAdopt = this.state.pets;
+    const adoptedPet = petsToAdopt.find(pet => pet.id === petId);
+    // const adoptedPetIndex = petsToAdopt.indexOf(adoptedPet); (This works, but it isn't needed.)
+
     adoptedPet.isAdopted = true;
-    console.log(adoptedPet);
+    // petsToAdopt.splice(adoptedPetIndex, 1, adoptedPet); (See commented code above.)
+
+    this.setState({
+      pets: petsToAdopt
+    });
   }
 
   render() {
