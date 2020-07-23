a8f757e705aa5652f3f343660b082c179889d3b3
sdcro <sdcrouse91@gmail.com>
2020-07-22 18:10:04 -0700

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -15,6 +15,38 @@ class App extends React.Component {
     }
   }
 
+  onChangeType = newType => {
+    this.setState({
+      filters: {
+        ...this.state.filters,
+        type: newType
+      }
+    })
+  }
+
+  findPets = () => {
+    let petsURL = '/api/pets';
+
+    if (this.state.filters.type !== 'all') {
+      petsURL += `?type=${this.state.filters.type}`;
+    }
+
+    fetch(petsURL)
+      .then(petsData => petsData.json())
+      .then(petsJson => {
+        this.setState({ pets: petsJson })
+      })
+      .catch(error => `Here's what went wrong: ${error}`);
+  }
+
+  adoptPet = petId => { 
+    // This technically passes the tests, but I think I need to update the state.
+    // Right now, the disabled adoption button won't render.
+    const adoptedPet = this.state.pets.find(pet => pet.id === petId);
+    adoptedPet.isAdopted = true;
+    console.log(adoptedPet);
+  }
+
   render() {
     return (
       <div className="ui container">
@@ -24,10 +56,10 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters onChangeType={this.onChangeType} onFindPetsClick={this.findPets} />
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser pets={this.state.pets} onAdoptPet={this.adoptPet} />
             </div>
           </div>
         </div>
