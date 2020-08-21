be4c1272b3ef68a16154a7a64ff0fcf6d8373f31
Kat Tochowicz <ktochow1@binghamton.edu>
2020-08-21 23:09:37 +0200

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
+import Pet from './Pet'
 import Filters from './Filters'
 import PetBrowser from './PetBrowser'
 
-class App extends React.Component {
+export default class App extends React.Component {
   constructor() {
     super()
     this.state = {
@@ -15,32 +16,49 @@ class App extends React.Component {
   }
 
 onFindPetsClick = () => {
-    if (this.state.filters.type === 'all'){ 
-    fetch(`/api/pets`)
-    .then(response => response.json())
-    .then(data => console.log(data))
-    } elsif (this.state.filters.type === 'cat'); {
-      fetch(`/api/pets/${'?type=cat'}`)
-      .then(response => response.json())
-      .then(data => console.log(data))
-    } elsif (this.state.filters.type === 'dog'); {
-      fetch(`/app/pets/${'?type=dog'}`)
-      .then(response => response.json())
-      .then(data => console.log(data))
-      } elsif (this.state.filters.type === 'micropig'); {
-        fetch(`/api/pets/${'?type=micropig'}`)
-        .then(response => response.json())
-        .then(data => console.log(data))
-      }
+    let endpoint = '/api/pets';
+
+    if (this.state.filters.type !== 'all') {
+      endpoint += `?type=${this.state.filters.type}`;
+    }
+
+    fetch(endpoint)
+      .then(res => res.json())
+      .then(pets => this.setState({ pets: pets }));
+  
+    // if (this.state.filters.type === 'all'){ 
+    // fetch(`/api/pets`)
+    // .then(response => response.json())
+    // .then(data => console.log(data))
+    // } elsif (this.state.filters.type === 'cat'); {
+    //   fetch(`/api/pets/${'?type=cat'}`)
+    //   .then(response => response.json())
+    //   .then(data => console.log(data))
+    // } elsif (this.state.filters.type === 'dog'); {
+    //   fetch(`/app/pets/${'?type=dog'}`)
+    //   .then(response => response.json())
+    //   .then(data => console.log(data))
+    //   } elsif (this.state.filters.type === 'micropig'); {
+    //     fetch(`/api/pets/${'?type=micropig'}`)
+    //     .then(response => response.json())
+    //     .then(data => console.log(data))
+    //   }
 }
 
-  onChangeType = event => {
+  onChangeType = ({target: {value}}) => {
     this.setState({
       filters: {
         ...this.state.filters,
-        type: event.target.value
+        type: value
       }
     })
+  }
+
+  onAdoptPet = (id) => {
+    const pets = this.state.pets.map(pet => {
+      return pet.id === id ? {...pet, isAdopted: true} : pet;
+    });
+    this.setState({pets: pets})
   }
   
 
@@ -53,11 +71,16 @@ onFindPetsClick = () => {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters onChangeType={this.onChangeType} onFindPetsClick={this.onFindPetsClick}/>
-              {/* <Filters /> */}
+              <Filters
+              onChangeType={this.onChangeType} 
+              onFindPetsClick={this.onFindPetsClick}
+              />
+              
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser pets={this.state.pets} onAdoptPet={this.onAdoptPet}
+              />
+              <Pet onAdoptPet={this.onAdoptPet}/>
             </div>
           </div>
         </div>
@@ -65,4 +88,7 @@ onFindPetsClick = () => {
     )
   }
 
-export default App
+
+
+
+}
--- src/components/Filters.js
+++ src/components/Filters.js
@@ -8,7 +8,7 @@ class Filters extends React.Component {
       <div className="ui form">
         <h3>Animal type</h3>
         <div className="field">
-          <select onChange={event => this.props.onChangeType(event)} name="type" id="type">
+          <select onChange={this.props.onChangeType} name="type" id="type">
             <option value="all">All</option>
             <option value="cat">Cats</option>
             <option value="dog">Dogs</option>
@@ -17,7 +17,7 @@ class Filters extends React.Component {
         </div>
 
         <div className="field">
-          <button onClick={event => this.props.onFindPetsClick(event)} className="ui secondary button">Find pets</button>
+          <button onClick={this.props.onFindPetsClick} className="ui secondary button">Find pets</button>
         </div>
       </div>
     )
