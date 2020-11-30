2c2c29baf5f8905d1d5c97d3aaaafe8eea9763e8
Sara Harvey <saraharveynyc@gmail.com>
2020-11-30 13:41:37 +0000

    Done.

--- src/components/Pet.js
+++ src/components/Pet.js
@@ -1,29 +1,52 @@
 import React from 'react'
 
-class Pet extends React.Component {
-  render() {
-    return (
-      <div className="card">
-        <div className="content">
-          <a className="header">
-            {/*'♀' OR '♂' */}
-            PET NAME
-          </a>
-          <div className="meta">
-            <span className="date">PET TYPE</span>
-          </div>
-          <div className="description">
-            <p>Age: PET AGE</p>
-            <p>Weight: PET WEIGHT</p>
-          </div>
+const Pet = ({ pet, onAdoptPet }) => {
+  //const { onAdoptPet } = props
+  // destructure this.props.pet
+  const { 
+    id, 
+    gender, 
+    age, 
+    name, 
+    type, 
+    weight, 
+    isAdopted 
+  } = pet
+
+  const alreadyAdoptedButton = <button className="ui disabled button">Already adopted</button>
+  const adoptPetButton = <button className="ui primary button" onClick={() => onAdoptPet(id)}>Adopt pet</button>
+  //const button = isAdopted ? alreadyAdoptedButton : adoptPetButton
+  
+  return (
+    <div className="card">
+      <div className="content">
+        <a className="header" href="/">
+          {/*'♀' OR '♂' */}
+          {gender === 'female' ? '♀' : '♂'}
+          {name}
+        </a>
+        <div className="meta">
+          <span className="date">{type}</span>
         </div>
-        <div className="extra content">
-          <button className="ui disabled button">Already adopted</button>
-          <button className="ui primary button">Adopt pet</button>
+        <div className="description">
+          <p>Age: {age}</p>
+          <p>Weight: {weight}</p>
         </div>
       </div>
-    )
-  }
+      <div className="extra content">
+        {isAdopted ? alreadyAdoptedButton : adoptPetButton}
+        {/* button */}
+        {/* 
+        {isAdopted === true &&
+          <button className="ui disabled button">Already adopted</button>
+        }
+        {isAdopted === false &&
+          <button className="ui primary button" onClick={() => onAdoptPet(id)}>Adopt pet</button>
+        }
+        */}
+      </div>
+    </div>
+  )
 }
 
 export default Pet
